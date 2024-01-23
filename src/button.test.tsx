df1976a794bd1054c99c4ecf1868ffbad2eb2891
Hendrik de Graaf <hendrikdegraaf@gmail.com>
2024-01-24 00:28:55 +0100

    chore: remove log statement from button

--- src/button.test.tsx
+++ src/button.test.tsx
@@ -6,6 +6,6 @@ describe('Button component', () => {
     it('Button should render correctly', () => {
         render(<Button numb={3} label="test" />)
         const button = screen.getByRole('button')
-        expect(button).toBeInTheDocument()
+        expect(button).toBeVisible()
     })
 })
--- src/button.tsx
+++ src/button.tsx
@@ -1,13 +1,12 @@
-import { useEffect } from 'react'
-
 type ButtonProps = {
     label: string
     numb: number
 }
 
 export function Button({ label, numb }: ButtonProps) {
-    useEffect(() => {
-        console.log(numb)
-    }, [numb])
-    return <button>{label}</button>
+    return (
+        <button>
+            {label} - {numb}
+        </button>
+    )
 }

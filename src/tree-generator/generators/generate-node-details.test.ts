836a2e600c7d57f6c3beb0e166417f48313d5a67
Hendrik de Graaf <hendrikdegraaf@gmail.com>
2026-04-05 23:28:10 +0200

    chore: fix uuid mocks

--- src/tree-generator/generators/generate-node-details.test.ts
+++ src/tree-generator/generators/generate-node-details.test.ts
@@ -1,6 +1,4 @@
-import * as uniqueNamesGeneratorExports from 'unique-names-generator'
-import * as uuidExports from 'uuid'
-import { afterAll, beforeAll, describe, expect, test, vi } from 'vitest'
+import { describe, expect, test, vi } from 'vitest'
 import { DisplayNameType } from '../prompts/display-name-type-prompt'
 import { IdType } from '../prompts/id-type-prompt'
 import { createNodeDetailsGenerator } from './generate-node-details'
@@ -8,6 +6,22 @@ import { createNodeDetailsGenerator } from './generate-node-details'
 type UuidMock = { v4: () => string }
 type UniqueNamesGeneratorMock = { uniqueNamesGenerator: () => string }
 type TestCase = [DisplayNameType, IdType]
+
+vi.mock('uuid', async (importOriginal) => {
+    const original: UuidMock = await importOriginal()
+    return {
+        ...original,
+        v4: () => 'mock-uuid-value',
+    }
+})
+
+vi.mock('unique-names-generator', async (importOriginal) => {
+    const original: UniqueNamesGeneratorMock = await importOriginal()
+    return {
+        ...original,
+        uniqueNamesGenerator: () => 'A Mocked Name',
+    }
+})
 
 const testCases: TestCase[] = [
     ['name', 'int'],
@@ -19,29 +33,6 @@ const testCases: TestCase[] = [
 ]
 
 describe('Node Details Generator', () => {
-    beforeAll(() => {
-        vi.spyOn(uuidExports as UuidMock, 'v4').mockImplementation(
-            () => 'mock-uuid-value'
-        )
-        vi.spyOn(uniqueNamesGeneratorExports, 'uniqueNamesGenerator')
-        vi.mock('uuid', async (importOriginal) => {
-            const original: UuidMock = await importOriginal()
-            return {
-                ...original,
-                v4: () => 'mock-uuid-value',
-            }
-        })
-        vi.mock('unique-names-generator', async (importOriginal) => {
-            const original: UniqueNamesGeneratorMock = await importOriginal()
-            return {
-                ...original,
-                uniqueNamesGenerator: () => 'A Mocked Name',
-            }
-        })
-    })
-    afterAll(() => {
-        vi.restoreAllMocks()
-    })
     test.each(testCases)(
         'Name type = %s \t ID type = %s',
         (nameType, idType) => {
